de71c98fc5457a1c384e37e46d5ff1ee27c848d6
KunHung Chi <kunhungg@gmail.com>
2018-05-08 21:34:27 +0800

    20180508V10

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express();
 
 const linebotParser = bot.parser();
 
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
 app.post('/linewebhook', linebotParser);
 
 bot.on('message', function(event) {
